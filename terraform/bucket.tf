c87f65198ce27a77d433107959d0b1c22cbcfbb5
asaf527 <asafkarako@outlook.co.il>
2025-02-19 09:51:18 +0200

    test

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,10 +1,4 @@
 # Create a separate bucket.tf file
-provider "aws" {
-  region = "us-east-1"
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key 
-}
-
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "regtech-iac"
   force_destroy = true
